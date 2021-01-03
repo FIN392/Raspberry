7323f1a78068ca43840ef45453dcb979d2511e60
Fin392 <fin392@gmail.com>
2021-01-03 12:27:56 +0100

    Delete reboot.sh

--- Projecto FIN392/telegramMonitoring/botCommands/reboot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#HELP: Reboot NOW
-echo "%F0%9F%98%B1"
-echo "I'm going to reboot in 1 minute! If everything goes well, I'll be back in 2 minutes"
-sudo shutdown --reboot +1 > /dev/null 2>&1
