fdf7d8256cd4dd939823a73221e4399ce458c1f3
Fin392 <fin392@gmail.com>
2021-01-03 12:27:52 +0100

    Delete downtime.sh

--- Projecto FIN392/telegramMonitoring/botCommands/downtime.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#HELP: List of last downtime periods
-echo "%F0%9F%9A%AB"
-cat /var/log/messages | awk /'Booting Linux/ {printf "%s %s %s OFF\n%s %s %s ON\n\n",a,b,c,$1,$2,$3}{a=$1}{b=$2}{c=$3}'
